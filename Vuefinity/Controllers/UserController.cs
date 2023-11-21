5391d83d222883d104e0b062de135e62dd7d052a
ErlingMMM <emagnusmoland@gmail.com>
2023-11-21 10:51:53 +0100

    user controller with PostExercise

---
+++ Vuefinity/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+using Vuefinity.Data.Exceptions;
+using Vuefinity.Data.Models;
+using Vuefinity.Services.Exercises;
+using Vuefinity.Data.DTO.Exercise;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Vuefinity.Controllers
+{
+    [ApiController]
+    [Route("api/v1/User")]
+    [Produces(MediaTypeNames.Application.Json)]
+    [ApiConventionType(typeof(DefaultApiConventions))]
+
+    //These files define the API endpoints, their routes, and the actions to be taken for each endpoint.
+    //Controllers interact with services to perform business logic.
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly IMapper _mapper;
+
+        public UserController(IUserService userService, IMapper mapper)
+        {
+            _userService = userService;
+            _mapper = mapper;
+        }
+
+
+    
+        /// <summary>
+        /// Creating a new exercise to the database
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<ActionResult<UserDTO>> PostExercise(UserPostDTO exercise)
+        {
+            var newExercise = await _userService.AddAsync(_mapper.Map<User>(exercise));
+
+            return CreatedAtAction("GetExercise",
+                new { id = newExercise.Id },
+                _mapper.Map<UserDTO>(newExercise));
+        }
+
+       
+    }
+}
