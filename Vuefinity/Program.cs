ce537d4b4077595070a9a1fb5aa8d5948e7ca863
ErlingMMM <emagnusmoland@gmail.com>
2023-11-21 13:45:37 +0100

    updated program class

--- Vuefinity/Program.cs
+++ Vuefinity/Program.cs
@@ -1,25 +1,90 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Vuefinity.Services.Users;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
 
-// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace Vuefinity.Data
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Vuefinity API",
+                    Description = "API for game data",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Annette & Erling",
+                        Url = new Uri("https://example.com/contact")
+                    }
+                });
+                // using System.Reflection;
+                var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+            });
+
+
+            builder.Services.AddDbContext<VuefinityDdContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("Vuefinity")));
+            // Add our service
+            builder.Services.AddScoped<IUserService, UserService>();
+          
+            // Add automapper
+            builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            builder.Services.AddAutoMapper(typeof(UserService));
+  
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger(options =>
+                {
+                    options.SerializeAsV2 = true;
+                }
+                );
+                app.UseSwaggerUI(options =>
+                {
+                    app.UseSwaggerUI(c =>
+                    {
+                        c.RoutePrefix = "swagger";
+                        c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                    });
+                });
+            }
+
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
